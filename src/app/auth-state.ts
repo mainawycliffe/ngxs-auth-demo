7c9d943554ae95665f05a61e6e128f741b2080bd
Maina Wycliffe <12270550+MainaWycliffe@users.noreply.github.com>
2018-07-25 20:55:22 +0300

    Update auth-state.ts

--- src/app/auth-state.ts
+++ src/app/auth-state.ts
@@ -40,13 +40,13 @@ export class AuthState {
   ) {
     return this.authService.signin(email, password).pipe(
       tap(result => {
-        console.log(
+        
           patchState({
             token: result.token,
             name: result.name,
             email: result.email
-          })
-        );
+          });
+        
       })
     );
   }
